36e3868cfef4da4d24fc1eb24a41c06d9bf641fd
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-17 23:38:55 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,3 @@
-local Namecall
-Namecall = hookmetamethod(game, "__namecall", function(self, ...)
-   if getnamecallmethod() == "FireServer" and tostring(self) == "Ban" then
-       return
-   elseif getnamecallmethod() == "FireServer" and tostring(self) == "WalkSpeedChanged" then
-       return
-   elseif getnamecallmethod() == "FireServer" and tostring(self) == "AdminGUI" then
-       return
-   elseif getnamecallmethod() == "FireServer" and tostring(self) == "GRAB" then
-       return
-   end
-   return Namecall(self, ...)
-end)
-
 local Players = game:GetService("Players")
 local StarterPlayer = game:GetService("StarterPlayer")
 
@@ -27,4 +13,35 @@ local function checkAndDestroyAntiMobileExploits()
     end
 end
 
+if hookmetamethod then
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Error",Text = "hookmetamethod success." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = ":)"})
+local Namecall
+Namecall = hookmetamethod(game, "__namecall", function(self, ...)
+   if getnamecallmethod() == "FireServer" and tostring(self) == "Ban" then
+       return
+   elseif getnamecallmethod() == "FireServer" and tostring(self) == "WalkSpeedChanged" then
+       return
+   elseif getnamecallmethod() == "FireServer" and tostring(self) == "AdminGUI" then
+       return
+   elseif getnamecallmethod() == "FireServer" and tostring(self) == "GRAB" then
+       return
+   end
+   return Namecall(self, ...)
+end)
+
+checkAndDestroyAntiMobileExploits()
+else
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Error",Text = "hookmetamethod failed." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = ":)"})
+if game:GetService("ReplicatedStorage"):FindFirstChild("Ban") then
+game:GetService("ReplicatedStorage").Ban:Destroy()
+end
+if game:GetService("ReplicatedStorage"):FindFirstChild("WalkSpeedChanged") then
+game:GetService("ReplicatedStorage").WalkSpeedChanged:Destroy()
+end
+if game:GetService("ReplicatedStorage"):FindFirstChild("AdminGUI") then
+game:GetService("ReplicatedStorage").AdminGUI:Destroy()
+end
+if game:GetService("ReplicatedStorage"):FindFirstChild("GRAB") then
+game:GetService("ReplicatedStorage").GRAB:Destroy()
+end
 checkAndDestroyAntiMobileExploits()
